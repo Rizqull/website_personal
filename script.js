8c3dea6de7404f17d574f40b39041147af5b2abc
Rizqull <84425347+Rizqull@users.noreply.github.com>
2025-09-04 02:27:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Menampilkan kejutan ketika tombol diklik
+function showSurprise() {
+  const surpriseMessage = document.getElementById("surpriseMessage");
+  surpriseMessage.style.display = "block";
+  surpriseMessage.textContent =
+    "Semoga sukses selalu, dan semoga impianmu tercapai! 🎂🎉";
+}
